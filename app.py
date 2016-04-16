c579f8c31fc3fac24f9f14003dfaf9269b5db88f
theshteves <kneisers@msu.edu>
2016-04-16 17:45:36 -0500

    First passing build

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
-app = Flask(__name__, static_folder='static', template_folder='template')
+#!/usr/bin/python3
+
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def serve_index():
-  return render_template('./app/template/index.html')
+def root():
+    #print("PATH: " + path)
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-  app.run()
-  
+    app.run(debug=True)
   

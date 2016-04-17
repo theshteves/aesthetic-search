da6ec81e33a1b1914832843b1f3f524400e3459e
Joshua Miles <milesjos@msu.edu>
2016-04-16 23:23:09 -0500

    Added functionality for the giphy api and pulling the trending gifs

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import urllib2
+import json
 
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 app = Flask(__name__, static_url_path='')
@@ -6,7 +8,14 @@ app = Flask(__name__, static_url_path='')
 @app.route('/')
 def root():
     #print("PATH: " + path)
-    return render_template('index.html')
+
+    r = urllib2.urlopen("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC").read()
+    parsed_json = json.loads(r)
+    data = parsed_json['data']
+    input_info = []
+    for gif in data:
+        input_info.append([gif['images']['downsized_medium'], gif['slug']])
+    return render_template('index.html',gifs=input_info)
 
 if __name__ == '__main__':
     app.run(debug=True)

fd3dd6117586d2bc2614ec164ee2cbae62c79581
theshteves <kneisers@msu.edu>
2016-04-16 23:14:43 -0500

    Merged Conflicts & Fixed Procfile

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from flask import Flask, request, redirect, url_for, send_from_directory, render
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
-def root():
+def hello():
     #print("PATH: " + path)
     return render_template('index.html')
+
+@app.route('/<path:path>')
+def static_file(path):
+        return app.send_static_file(path)
 
 if __name__ == '__main__':
     app.run(debug=True)
